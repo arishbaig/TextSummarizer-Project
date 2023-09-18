a6e00e959d41f509f082e12f911070eaba999da8
Arish Baig <arishbaig@gmail.com>
2023-09-18 10:07:15 -0400

    updated logging module

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.1"
+
+REPO_NAME = "TextSummarizer-Project"
+AUTHOR_USERNAME = "arishbaig"
+SRC_REPO = "TextSummarizer
+AUTHOR_EMAIL = "arishbaig@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USERNAME,
+    author_email=AUTHOR_EMAIL,
+    description="Text Summarizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/{AUTHOR_USERNAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": "https://github.com/{AUTHOR_USERNAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
